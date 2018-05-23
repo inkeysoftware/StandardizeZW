8261550d4f4c5b33f3e44f911a9735c11a230c0b
anitabeniston <35677452+anitabeniston@users.noreply.github.com>
2018-05-23 19:49:22 +0530

    Add files via upload

--- AnalyzeZeroWidthCharacters.py
+++ AnalyzeZeroWidthCharacters.py
@@ -1,10 +1,12 @@
 # coding=utf-8
 from ScriptureObjects import ScriptureText
+from CommonZWC import countChanges
 import re
 import sys
 import codecs
 import os
 import shutil
+import difflib
 
 #__________________________________________________________________________________
 # INITIALIZE CONSTANTS WITH REGEX STRINGS:
@@ -29,50 +31,14 @@ zw = u'[\u200c\u200d]*'
 # cluster: This is our definition of an orthographic consonant cluster
 cluster =  '(?:' + c + optNukta + virama + zw + ')+(?:' + c + optNukta + ')?'
 
+# Initialization variable
+default = 0
+
 #__________________________________________________________________________________
 # INITIALIZE OTHER CONSONANTS AND VARIABLES:
 
+
 clusFilename = SettingsDirectory + Project + "\\ClusterStatus.TXT"    # New output to support multiple valid forms
-
-#__________________________________________________________________________________
-def countChanges(aStr, bStr):  
-# Returns a tally of the differences between two strings, and ensures that the only differences are ZWJ/ZWNJ.
-# Alerts us that we have a bug if we've actually made changes other than to ZW chars.
-
-    global abort
-    a = 0               # index as we walk through aStr
-    b = 0               # index as we walk through bStr
-    aLen = len(aStr)
-    bLen = len(bStr)
-    changes = 0         # tally of changes encountered
-    zws = u'\u200c\u200d'  # the ZW chars
-    while 1:            
-        if a == aLen and b == bLen:  # If we've reached the end of both strings at the same time,
-            return changes           #   return the number of changes tallied so far.
-        if a == aLen or b == bLen:   # If we've reached the end of one string before the other,
-            return changes + aLen - a + bLen - b  # return number of changes so far plus number of leftover uncompared chars.
-            # TODO: Verify that the leftover chars are indeed all ZW chars, not the result of accidental deletion of the end of a string!
-            
-        if aStr[a] == bStr[b]:       # If both strings contain the same character at this comparison point,
-            a += 1                   #     increment our index in both strings
-            b += 1                   #     and continue to the next pair.
-            continue
-        if ((aStr[a] in zws) and (bStr[b] in zws)): # If both strings contain a ZW char, but they are different from each other,
-            changes += 1                            #     add one to the changes tally,
-            a += 1                                  #     increment our index in both strings,
-            b += 1                                  #     and continue to the next pair.
-            continue
-        if aStr[a] in zws:    # If our index position in aStr contains a ZW,
-            changes += 1      #    add one to the changes tally,
-            a += 1            #    increment our position in aStr,
-            continue          #    and continue to the next pair.
-        if bStr[b] in zws:    # Likewise for bStr.
-            changes += 1
-            b += 1
-            continue
-        # If none of these were the case, the strings differ by something other than a ZW character! Check what we did wrong!!
-        sys.stderr.write("ERROR: This script was supposed to ignore invalid ZW characters, but the comparison is failing: " + repr(aStr[a]) + "!" + repr(bStr[b]) + "\n")
-    return 0
 
 
 #__________________________________________________________________________________
@@ -130,11 +96,8 @@ def initialize():
                         
             # Find the examples and the clusters
             for example in re.findall('[\p{L}\p{M}\p{Cf}]+', text2):
-                #clusFile.write(example)
                 for cl in re.findall(cluster, example):
-                    #sys.stderr.write(cl)
                     if cl not in listWords:
-                        #listWords.update({cl:example})
                         listWords[cl] = [example]
                     else:
                         # if example not in list values - append unique values
@@ -160,15 +123,32 @@ def initialize():
 
 
 def examples(bCluster):
-    global listWords
-          
+    global listWords      
     return ', '.join(sorted(listWords[bCluster]))
     
-    ##### MAIN PROGRAM #####
+def examplesList(cCluster):
+    global ExampleCount
+    sampleList = re.split(',',examples(cCluster))
+       
+    finalList = []
+
+    for example in sampleList:
+        if len(finalList) == ExampleCount:
+            return finalList
+        else:
+            finalList.append(example);
+            for each in difflib.get_close_matches(example,sampleList):
+                sampleList.remove(each)
+           
+    return finalList
+
+   ##### MAIN PROGRAM #####
 
 formTally = {} # A hash array of hash arrays to tally the frequency count for each form of each combination of consonants
 clusCt = 0
 invalidReport = ""
+
+
 if initialize():
     for base in sorted(formTally.iterkeys()):
         root = re.sub(virama, '', base)
@@ -178,13 +158,15 @@ if initialize():
         bestForm = sortedForms[0]
         bestFormShow = showAll(bestForm)
                 
-        clusFile.write(root + "\t" + bestForm + "\t" + bestFormShow + "\t" + str(int(formTally[base][bestForm])) + "\t" + "\t" + "\t" + examples(bestForm)) 
+        #clusFile.write(root + "\t" + bestForm + "\t" + bestFormShow + "\t" + str(int(formTally[base][bestForm])) + "\t" + "\t" + "\t" + tamedEglist(bestForm,5)) 
+        clusFile.write(root + "\t" + bestForm + "\t" + bestFormShow + "\t" + str(int(formTally[base][bestForm])) + "\t" + "\t" + "\t")
+        clusFile.write(','.join(examplesList(bestForm)))
         clusFile.write("\r\n")
         clusCt += 1
         
         # Now write out all remaining forms
         for x in range(1, len(sortedForms)):
-            clusFile.write(root + "\t" + sortedForms[x] + "\t" + showAll(sortedForms[x]) + "\t" + str(int(formTally[base][sortedForms[x]])) + "\t" + bestForm + "\t" + bestFormShow + "\t" + examples(sortedForms[x])) 
+            clusFile.write(root + "\t" + sortedForms[x] + "\t" + showAll(sortedForms[x]) + "\t" + str(int(formTally[base][sortedForms[x]])) + "\t" + bestForm + "\t" + bestFormShow + "\t" + examplesList(sortedForms[x])) 
             clusFile.write("\r\n") 
             clusCt += 1
             
