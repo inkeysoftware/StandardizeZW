9c582dc2c2a3d5493236d79f584d87e10211815c
anitabeniston <35677452+anitabeniston@users.noreply.github.com>
2018-07-11 12:13:51 +0530

    Add files via upload

--- AnalyzeZeroWidthCharacters.py
+++ AnalyzeZeroWidthCharacters.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from ScriptureObjects import ScriptureText
-from CommonZWC import countChanges
+from CommonZWC import *
 import re
 import sys
 import codecs
@@ -8,28 +8,6 @@ import os
 import shutil
 import difflib
 
-#__________________________________________________________________________________
-# INITIALIZE CONSTANTS WITH REGEX STRINGS:
-
-# consonants
-cCodes = u'\u0915-\u0939\u0958-\u095f\u097b-\u097f' + u'\u0995-\u09b9\u09ce\u09dc-\u09df\u09f0-\u09f1' + u'\u0a15-\u0a39\u0a59-\u0a5f' + u'\u0a95-\u0ab9' + u'\u0b15-\u0b39\u0b5c-\u0b5f\u0b71' + u'\u0b95-\u0bb9' + u'\u0c15-\u0c39\u0c58\u0c59' + u'\u0c95-\u0cb9\u0cde' + u'\u0d15-\u0d39\u0d7a-\u0d7f'
-c = '[' + cCodes + ']'          # c = set of all consonant characters, from each Indic script
-nonCons = '[^' + cCodes + ']'   # nonCons = set of all characters that are not Indic consonants. 
-
-# nukta
-nuktaCodes = u'\u093c\u09bc\u0a3c\u0abc\u0b3c\u0bbc\u0c3c\u0cbc\u0d3c' # includes some yet-to-be adopted nuktas.
-optNukta = '[' + nuktaCodes + ']*' # zero or more nukta characters
-
-# The character class of all scripts' viramas, and the class of everything that is NOT a virama.
-viramaCodes = u'\u094d\u09cd\u0a4d\u0acd\u0b4d\u0bcd\u0c4d\u0ccd\u0d4d'
-virama = '[' + viramaCodes + ']'
-notVirama = '([^' + viramaCodes + '])'
-
-# zw = any number of optional ZWJ or ZWNJ
-zw = u'[\u200c\u200d]*'
-
-# cluster: This is our definition of an orthographic consonant cluster
-cluster =  '(?:' + c + optNukta + virama + zw + ')+(?:' + c + optNukta + ')?'
 
 # Initialization variable
 default = 0
@@ -37,14 +15,8 @@ default = 0
 #__________________________________________________________________________________
 # INITIALIZE OTHER CONSONANTS AND VARIABLES:
 
-
-clusFilename = SettingsDirectory + Project + "\\ClusterStatus.TXT"    # New output to support multiple valid forms
-
-
-#__________________________________________________________________________________
-def showAll(s):
-# Return a version of the string that shows [zwj] and [zwnj] in place of invisible characters
-    return re.sub(u'\u200c', '[zwnj]', re.sub(u'\u200d', '[zwj]', s))
+clusFilename = SettingsDirectory + Project + "\\ClusterCorrections.TXT"    # New output to support multiple valid forms
+clusallFilename = SettingsDirectory + Project + "\\AllClusterCorrections.TXT"    # New output to support multiple valid forms
 
 #__________________________________________________________________________________
 def weightedCt(cl, base):
@@ -67,28 +39,28 @@ def initialize():
 # Open file for output, and read all books to tally the frequency of each form of each cons combination.
 # Return true if successful.
 
-    global clusFilename, invalidReport, formTally, bases, thisVirama, f, clusFile, notVirama, v, virama, listWords, cl
+    global clusFilename, clusallFilename, invalidReport, formTally, bases, thisVirama, f, clusFile, clusallFile, notVirama, v, virama, listWords, cl, clDict
 
     try:
         clusFile = codecs.open(clusFilename, mode='w', encoding='utf-8')
         clusFile.write(u'\uFEFFRoot\tCluster\tClusterShow\tCount\tCorrect\tCorrectShow\tExamples\r\n') # BOM and column headings
-        #TODO: Add an Examples column header
+        
     except Exception, e:
         sys.stderr.write("Unable to write to file: " + clusFilename + "\n")
         return 0
-      
+    
+          
     try:
         scr = ScriptureText(Project)     # Open input project
-        
-        
+         
         listWords = {}
         
+                
         for reference, text in scr.allBooks(Books):  
         
             # Ignore any invalid ZW characters. THE REGEXES IN THIS CODE SHOULD MATCH WHAT'S IN THE STANDARDIZE SCRIPT!
-            text2 = re.sub(notVirama + u'[\u200c\u200d]+', r'\1', text)  # Remove any ZW that doesn't follow virama
-            text2 = re.sub('(' + nonCons + optNukta + virama + ')' + u'[\u200c\u200d]+', r'\1', text2) # Remove ZW that follows a weird virama that follows a non-Consonant.
-                                
+            text2 = ignoreanyInvalidzw(text)
+                                 
             # Keep track of invalid ZW characters removed, so we can report on that at the end.
             invalidCt = countChanges(text, text2)
             if (invalidCt > 0):
@@ -121,56 +93,141 @@ def initialize():
     
     return 1    
 
-
 def examples(bCluster):
     global listWords      
-    return ', '.join(sorted(listWords[bCluster]))
-    
-def examplesList(cCluster):
     global ExampleCount
-    sampleList = re.split(',',examples(cCluster))
-       
-    finalList = []
 
-    for example in sampleList:
-        if len(finalList) == ExampleCount:
-            return finalList
-        else:
-            finalList.append(example);
-            for each in difflib.get_close_matches(example,sampleList):
-                sampleList.remove(each)
-           
-    return finalList
+    s = sorted(listWords[bCluster])
+    ls = len(s)
+    if ExampleCount >= ls:  # If they want more examples than we have, give all we have.
+        return ', '.join(s)
+        
+    ExampleCount = max(ExampleCount,2)
+    
+    # Create a list containing the examples that were first, last, and equally-spaced between first and last.
+    e = s[0:1]  # First item
+    for i in range(1,ExampleCount):
+        e.append(s[int(0.5 + (ls-1.0)/(ExampleCount-1.0) * i)]) # ExampleCount-1 additional items
+    return ', '.join(e)
 
-   ##### MAIN PROGRAM #####
+def buildDict():
+
+    global  clusallFilename, clusallFile, clDict
+    #clDict = {}
+    
+    fileread = 0
+    try:
+        if os.path.isfile(clusallFilename):
+            clusallFile = codecs.open(clusallFilename, encoding='utf-8')
+            allfilecontents = clusallFile.read()
+            fileread = 1
+            
+    except Exception, e:
+        sys.stderr.write("Unable to read or write to file: " + clusallFilename + "\n")
+        return 0
+    
+    try:  
+        
+        if (fileread):   
+            
+            allfileLines = re.split(r' *[\r\n]+[\s\r\n]*', allfilecontents)   # Split file on newlines (eating any leading or trailing spaces)
+            
+            
+            for x in range(1, len(allfileLines)):                          # For each of the remaining lines,
+                allfields = re.split(r' *\t *', allfileLines[x])              # Split line on tabs into fields.
+                #sys.stderr.write(allfields[0])
+                if len(allfields)> 3:
+                    
+                    #Store all clusters in allCluster dictionary
+                    clDict[allfields[0]] = {}
+                    #sys.stderr.write("before creating")
+                    #if allfields[0] in clDict.iterkeys():
+                    #    clDict[allfields[0]].append(fields[1])
+                    #else:   
+                    #clDict[allfields[0]] = fields[1]
+                    for x1 in range(1, len(allfileLines)):
+                        fields1 = re.split(r' *\t *', allfileLines[x1])
+                        #counter = counter + 1
+                        if len(fields1)> 3:
+                            #sys.stderr.write("in for loop clDict" + str(int(counter)))
+                            #if fields1[0] not in clDict.iterkeys():
+                            #sys.stderr.write("in for loop clDict")
+                            if allfields[1]==fields1[1]: 
+                                clDict[allfields[0]][fields1[1]] = fields1[4] + ";" + fields1[5]
+                                #sys.stderr.write("in for loop clDict")
+                    #sys.stderr.write("after assigning")                       
+    except Exception, e:
+        sys.stderr.write("Unable to process all cluster file")
+        return 0 # Error
+    
+    clusallFile.close()
+    
+    return clDict
+    
+def checkExists(bs, findCluster):
+    global clDict
+    # Read the contents of the input file into filecontents
+    
+    if os.path.isfile(clusallFilename):
+        #sys.stderr.write(bs + findCluster)
+        #sys.stderr.write("file exists")
+        if bs in clDict.iterkeys():
+            #sys.stderr.write("keys exist here")
+            if findCluster in clDict[bs].iterkeys:
+                #sys.stderr.write("In here")
+                return 1
+     
+    return 0  
+    
+
+##### MAIN PROGRAM #####
 
 formTally = {} # A hash array of hash arrays to tally the frequency count for each form of each combination of consonants
 clusCt = 0
 invalidReport = ""
-
+clDict = {}
+checkval = 0
 
 if initialize():
+    # Build allclusters dictionary from allclustercorrections file
+    clDict = buildDict()
+        
     for base in sorted(formTally.iterkeys()):
         root = re.sub(virama, '', base)
         sortedForms = sorted(formTally[base].iterkeys(), key=lambda a: formTally[base][a], reverse=True) # Sort from most frequent to least
         
-        # First write out best form
-        bestForm = sortedForms[0]
-        bestFormShow = showAll(bestForm)
-                
-        #clusFile.write(root + "\t" + bestForm + "\t" + bestFormShow + "\t" + str(int(formTally[base][bestForm])) + "\t" + "\t" + "\t" + tamedEglist(bestForm,5)) 
-        clusFile.write(root + "\t" + bestForm + "\t" + bestFormShow + "\t" + str(int(formTally[base][bestForm])) + "\t" + "\t" + "\t")
-        clusFile.write(','.join(examplesList(bestForm)))
-        clusFile.write("\r\n")
-        clusCt += 1
+        if ExcludeSingle == "No" or len(sortedForms) > 1:
         
-        # Now write out all remaining forms
-        for x in range(1, len(sortedForms)):
-            clusFile.write(root + "\t" + sortedForms[x] + "\t" + showAll(sortedForms[x]) + "\t" + str(int(formTally[base][sortedForms[x]])) + "\t" + bestForm + "\t" + bestFormShow + "\t" + examplesList(sortedForms[x])) 
-            clusFile.write("\r\n") 
-            clusCt += 1
+            # First write out best form
+            bestForm = sortedForms[0]
+            bestFormShow = showAll(bestForm)
             
-        clusFile.write("\r\n")
+            #for a in sorted(clDict.iterkeys()):
+            #    sys.stderr.write(a)
+            if base in clDict.iterkeys():
+                #sys.stderr.write("in here")
+                if bestForm in clDict[base].iterkeys:
+                    checkval = 1
+                else: 
+                    checkval = 0
+                        
+            if checkval==0:
+                #clusFile.write(root + "\t" + bestForm + "\t" + bestFormShow + "\t" + str(int(formTally[base][bestForm])) + "\t" + "\t" + "\t" + tamedEglist(bestForm,5)) 
+                # clusFile.write(root + "\t" + bestForm + "\t" + bestFormShow + "\t" + str(int(formTally[base][bestForm])) + "\t" + "\t" + "\t")
+                clusFile.write(root + "\t" + bestForm + "\t" + bestFormShow + "\t" + str(int(formTally[base][bestForm])) + "\t" + "\t" + "\t" + examples(bestForm)) 
+                # clusFile.write(', '.join(examplesList(bestForm)))
+                clusFile.write("\r\n")
+                
+                clusCt += 1
+                
+                # Now write out all remaining forms
+                for x in range(1, len(sortedForms)):
+                    # clusFile.write(root + "\t" + sortedForms[x] + "\t" + showAll(sortedForms[x]) + "\t" + str(int(formTally[base][sortedForms[x]])) + "\t" + bestForm + "\t" + bestFormShow + "\t" + examplesList(sortedForms[x])) # should return a string
+                    clusFile.write(root + "\t" + sortedForms[x] + "\t" + showAll(sortedForms[x]) + "\t" + str(int(formTally[base][sortedForms[x]])) + "\t" + bestForm + "\t" + bestFormShow + "\t" + examples(sortedForms[x])) 
+                    clusFile.write("\r\n") 
+                    clusCt += 1
+                    
+                clusFile.write("\r\n")
         
 # Report on number of clusters written
 if (clusCt>0):
@@ -181,6 +238,7 @@ if (len(invalidReport) > 0):
     sys.stderr.write("\nAlso note: Using the STANDARDIZE tool to remove invalid ZW characters will fix this many issues:\n" + invalidReport + "\n")
     
 clusFile.close()
+clusallFile.close()
 
 # Copy the XLSX file to the project folder, if available and not already there.
 try:
--- StandardizeZeroWidthCharacters.py
+++ StandardizeZeroWidthCharacters.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from ScriptureObjects import ScriptureText
-from CommonZWC import countChanges
+from CommonZWC import *
 import re
 import sys
 import codecs
@@ -12,7 +12,7 @@ import shutil
 # See accompanying Help file for purpose and usage.
 
 # Phase 1: Removes ZWNJ/ZWJ found in invalid positions.
-# Phase 2: Applies cluster corrections found in ClusterStatus.txt to the text.
+# Phase 2: Applies cluster corrections found in ClusterCorrections.txt to the text.
 
 # TODO: We need to figure out how to handle SpellingStatus.xml. These changes should apply at least to the words marked as correct spellings, 
 # and also to corrections provided for incorrect spellings. If there are words that are marked as incorrect spellings, we shouldn't apply this to them,
@@ -22,40 +22,19 @@ import shutil
 # TODO: See if there's a way to apply this to the user's own Notes file, so that notes won't become detached from the word/context they are attached to when the spelling there changes. (Not essential, but would be nice.)
 
 #__________________________________________________________________________________
-# INITIALIZE CONSTANTS WITH REGEX STRINGS:
-
-# consonants
-cCodes = u'\u0915-\u0939\u0958-\u095f\u097b-\u097f' + u'\u0995-\u09b9\u09ce\u09dc-\u09df\u09f0-\u09f1' + u'\u0a15-\u0a39\u0a59-\u0a5f' + u'\u0a95-\u0ab9' + u'\u0b15-\u0b39\u0b5c-\u0b5f\u0b71' + u'\u0b95-\u0bb9' + u'\u0c15-\u0c39\u0c58\u0c59' + u'\u0c95-\u0cb9\u0cde' + u'\u0d15-\u0d39\u0d7a-\u0d7f'
-c = '[' + cCodes + ']'          # c = set of all consonant characters, from each Indic script
-nonCons = '[^' + cCodes + ']'   # nonCons = set of all characters that are not Indic consonants. 
-
-# nukta
-nuktaCodes = u'\u093c\u09bc\u0a3c\u0abc\u0b3c\u0bbc\u0c3c\u0cbc\u0d3c' # includes some yet-to-be adopted nuktas.
-optNukta = '[' + nuktaCodes + ']*' # zero or more nukta characters
-
-# The character class of all scripts' viramas, and the class of everything that is NOT a virama.
-viramaCodes = u'\u094d\u09cd\u0a4d\u0acd\u0b4d\u0bcd\u0c4d\u0ccd\u0d4d'
-virama = '[' + viramaCodes + ']'
-notVirama = '([^' + viramaCodes + '])'
-
-# zw = any number of optional ZWJ or ZWNJ
-zw = u'[\u200c\u200d]*'
-
-# cluster: This is our definition of an orthographic consonant cluster
-cluster =  '(?:' + c + optNukta + virama + zw + ')+(?:' + c + optNukta + ')?'
-
-#__________________________________________________________________________________
 # INITIALIZE OTHER CONSONANTS AND VARIABLES:
 
-infile = SettingsDirectory + Project + "\\ClusterStatus.TXT"  # The input filename
+infile = SettingsDirectory + Project + "\\ClusterCorrections.TXT"  # The input filename
+
+allclinfile = SettingsDirectory + Project + "\\AllClusterCorrections.TXT"    # New input to store previous changes
 
 #__________________________________________________________________________________
 def loadFromFile():
 # Initializes the correction hash by reading the input file.
 # Returns true if sucessful.
 
-    global infile, correction
-
+    global infile, allclinfile, correction, allCluster, alldata, clusallFile, examplemap, dict_items, clDict
+    
     # Read the contents of the input file into filecontents
     try:
         f = codecs.open(infile, encoding='utf-8')
@@ -64,28 +43,102 @@ def loadFromFile():
         sys.stderr.write("Unable to open file: " + infile + "\nPlease create this file using the Analyze Zero-Width Characters tool.")
         return 0
 
-    # Parse the filecontents to get cluster mappings
     try:
+        # Build allclusters dictionary from allclustercorrections file
+        clDict = buildDict()
+        
+        # Parse the filecontents of clustercorrections to get cluster mappings    
         fileLines = re.split(r' *[\r\n]+[\s\r\n]*', filecontents)   # Split file on newlines (eating any leading or trailing spaces)
         fields = re.split(r'\t', fileLines[0])                      # Split header line on tabs
         if fields[1] != 'Cluster' or fields[4] != "Correct":        # Make sure the headers match what we're expecting
             sys.stderr.write("Unexpected format in file: " + infile + "\nPlease re-create this file using the Analyze Zero-Width Characters tool.")
             return 0
-            
+        
         for x in range(1, len(fileLines)):                          # For each of the remaining lines,
+           
             fields = re.split(r' *\t *', fileLines[x])              # Split line on tabs into fields.
-            if len(fields) >= 5 and re.match(r'[\p{L}\p{M}\p{Cf}]+$', fields[4]):   # If there is a replacement field (consisting only of word-forming characters)
-                if re.sub(u'[\u200c\u200d]', '', fields[1]) == re.sub(u'[\u200c\u200d]', '', fields[4]):    # If cluster and its replacement differ only by ZW characters
-                    correction[fields[1]] = fields[4]                                                       # map the invalid cluster to its replacement.
-                else:
-                    sys.stderr.write("Ignoring excessive correction of " + repr(fields[1]) + " to " + repr(fields[4]) + "\n") # This tool must only make ZW changes!
             
+            # Read Clustercorrection file and store all clusters in allCluster dictionary - start
+            allCluster[fields[0]]={}
+            if len(fields) > 3:
+                for eachLine in range(1, len(fileLines)):
+                    columns = re.split(r' *\t *', fileLines[eachLine])
+                    if columns[0]==fields[0]: 
+                        allCluster[fields[0]][columns[1]] = columns[4] + ";" + columns[6]  
+                        
+            # Store all cluster in allCluster dictionary - ends  
+            
+                if len(fields) >= 5 and re.match(r'[\p{L}\p{M}\p{Cf}]+$', fields[4]):   # If there is a replacement field (consisting only of word-forming characters)
+                    if re.sub(u'[\u200c\u200d]', '', fields[1]) == re.sub(u'[\u200c\u200d]', '', fields[4]):    # If cluster and its replacement differ only by ZW characters
+                        correction[fields[1]] = fields[4]                                                       # map the invalid cluster to its replacement.
+                    else:
+                        sys.stderr.write("Ignoring excessive correction of " + repr(fields[1]) + " to " + repr(fields[4]) + "\n") # This tool must only make ZW changes!
+    
+        
     except Exception, e:
-        sys.stderr.write("Problem reading file: " + infile + "\n")
+        sys.stderr.write("Error reading file: " + infile + "\n")
         return 0 # Error
 
     return 1    # Success
 
+def buildDict():
+
+    global  allclinfile, clusallFile, clDict
+    #clDict = {}
+
+    fileread = 0
+    try:
+        if os.path.isfile(allclinfile):
+            clusallFile = codecs.open(allclinfile, encoding='utf-8')
+            allfilecontents = clusallFile.read()
+            fileread = 1
+        #else:
+            #clusallFile = codecs.open(allclinfile, encoding='utf-8')
+            #clusallFile.write(u'\uFEFFRoot\tCluster\tClusterShow\tCorrect\tCorrectShow\tExamples\r\n') # BOM and column headings
+            #fileread = 0
+    except Exception, e:
+        sys.stderr.write("Unable to read or write to file: " + allclinfile + "\n")
+        return 0
+    
+    try:    
+        if (fileread):   
+            
+            allfileLines = re.split(r' *[\r\n]+[\s\r\n]*', allfilecontents)   # Split file on newlines (eating any leading or trailing spaces)
+            allfields = re.split(r'\t', allfileLines[0])                      # Split header line on tabs
+            count = 0 
+            
+            for x in range(1, len(allfileLines)):                          # For each of the remaining lines,
+                allfields = re.split(r' *\t *', allfileLines[x])              # Split line on tabs into fields.
+                if len(allfields)> 3:
+                    #sys.stderr.write("in for loop clDict" + str(int(count)))
+                    #Store all clusters in allCluster dictionary
+                    clDict[allfields[0]] = {}
+                    count = count + 1
+                    counter = 0
+                    for x1 in range(1, len(allfileLines)):
+                        fields1 = re.split(r' *\t *', allfileLines[x1])
+                        counter = counter + 1
+                        if len(fields1)> 3:
+                            #sys.stderr.write("in for loop clDict" + str(int(counter)))
+                            #if fields1[0] not in clDict.iterkeys():
+                            #sys.stderr.write("in for loop clDict")
+                            if allfields[1]==fields1[1]: 
+                                clDict[allfields[0]][fields1[1]] = fields1[4] + ";" + fields1[5]
+                                
+                                if len(fields1) >= 4 and re.match(r'[\p{L}\p{M}\p{Cf}]+$', fields1[3]):   # If there is a replacement field (consisting only of word-forming characters)
+                                    if re.sub(u'[\u200c\u200d]', '', fields1[1]) == re.sub(u'[\u200c\u200d]', '', fields1[3]):    # If cluster and its replacement differ only by ZW characters
+                                        correction[fields1[1]] = fields1[3]                                                       # map the invalid cluster to its replacement.
+                                        
+                                    else:
+                                        sys.stderr.write("Ignoring excessive correction of " + repr(fields1[1]) + " to " + repr(fields1[3]) + "\n") # This tool must only make ZW changes!
+                
+    except Exception, e:
+        sys.stderr.write("Unable to process all cluster file")
+        return 0 # Error
+    
+    clusallFile.close()
+    
+    return clDict
 #__________________________________________________________________________________
 def prefCluster(matchobj):
 # Provides the standardized replacement wherever a cluster pattern has been matched.
@@ -110,8 +163,9 @@ def makeChanges(fileContents, bookName):
     
     # Phase 1: Remove ZW characters from places we don't think they should ever appear.
     if RemoveBadZWs=="Yes":     # If user has opted to use Phase 1:
-        phase1Text = re.sub(notVirama + u'[\u200c\u200d]+', r'\1', phase1Text)  # Remove any ZW that doesn't follow virama
-        phase1Text = re.sub('(' + nonCons + optNukta + virama + ')' + u'[\u200c\u200d]+', r'\1', phase1Text) # Remove ZW that follows a weird virama that follows a non-Consonant.
+        phase1Text = ignoreanyInvalidzw(phase1Text)
+        #phase1Text = re.sub(notVirama + u'[\u200c\u200d]+', r'\1', phase1Text)  # Remove any ZW that doesn't follow virama
+        #phase1Text = re.sub('(' + nonCons + optNukta + virama + ')' + u'[\u200c\u200d]+', r'\1', phase1Text) # Remove ZW that follows a weird virama that follows a non-Consonant.
 
         if phase1Text != fileContents:  # If these Phase 1 changes have made a difference:
             changeBadCt = countChanges(fileContents, phase1Text)    # Remember the tally of changes made to this book by Phase 1
@@ -136,7 +190,59 @@ def makeChanges(fileContents, bookName):
         sys.stderr.write(str(changeBadCt) + "\t" + str(changeStdCt) + "\t" + bookName + "\n")
     return phase2Text
 
+   
+# Write into the Allclustercorrections file
+def writetoFile():
 
+    global clusallFile, clDict, allCluster
+    #sys.stderr.write("in writetofile")
+    try:
+        if os.path.isfile(allclinfile):
+            clusallFile = codecs.open(allclinfile, mode='a', encoding='utf-8')
+            fileexists=1
+        else:
+            clusallFile = codecs.open(allclinfile, mode='w', encoding='utf-8')
+            clusallFile.write(u'\uFEFFRoot\tCluster\tClusterShow\tCorrect\tCorrectShow\tExamples\r\n') # BOM and column headings
+            fileexists=0
+    except Exception, e:
+        sys.stderr.write("Unable to append or write to file: " + allclinfile + "\n")
+        return 0
+    
+    for cl in sorted(allCluster.iterkeys()):
+        
+        for eachCluster in sorted(allCluster[cl].iterkeys()):
+            found = 0
+            cols = re.split(";", allCluster[cl][eachCluster])
+            bestForm = cols[0]
+            bestFormShow = showAll(cols[0])
+            examples = cols[1]
+            
+            if fileexists:
+                if cl in sorted(clDict.keys()):
+                    sys.stderr.write("\nIm found in base" + cl + eachCluster)
+                    if eachCluster in sorted(clDict[cl].keys()):
+                        found = 1
+                        sys.stderr.write(eachCluster + ":the cluster exists in allclustercorrections\n") #till here
+                '''
+                for eachCl in sorted(clDict.iterkeys()):
+                    if (clDict[eachCl].iterkeys()):
+                        for x1 in sorted(clDict[eachCl].iterkeys()):
+                            sys.stderr.write(eachCluster)
+                            sys.stderr.write(x1)
+                            if eachCluster==x1:
+                                found = 1
+                    '''   
+                    #if cl == eachCl:
+                    #    found = 1
+            if found==0:
+                clusallFile.write(cl + "\t" + eachCluster + "\t" + showAll(eachCluster) +   "\t" +  bestForm +  "\t" + bestFormShow + "\t" + examples ) 
+                clusallFile.write("\r\n")
+                
+        clusallFile.write("\r\n")
+      
+    return 1
+    
+    
 #__________________________________________________________________________________
 ##### MAIN PROGRAM #####
 
@@ -148,6 +254,9 @@ else:
     scrOut = ScriptureText(OutputProject)    # Open separate output project
 
 correction = {}             # Maps from an invalid form of a cluster to its correction
+allCluster = {}             # store all valid and invalid cluster combinations
+alldata = {}
+clDict = {}
 totBadChanges = 0           # tally of Phase 1 changes across all books
 totStdChanges = 0           # tally of Phase 2 changes across all books
 printedHeadingAlready = 0   # whether we've printed the column heading already
@@ -156,6 +265,7 @@ printedHeadingAlready = 0   # whether we've printed the column heading already
 if (StandardizeClusters=="Yes" and not loadFromFile()):
     sys.stderr.write("Skipping standardizing clusters.\n")
     StandardizeClusters = "No"
+    
 
 # First process the scripture books that the user has selected in the input project.
 # (Normally they should select "All books".)
@@ -164,6 +274,10 @@ for reference, text in scr.allBooks(Books):
     if text2 != text or Project != OutputProject:  # If text has changed, or if output project is different from input project,
         scrOut.putText(reference, text2)       #    save changed text out to file.
 scrOut.save(OutputProject)  # The books present might have changed so we need to update ssf file.
+
+# Create or append AllClusterCorrections file
+if not writetoFile():
+    sys.stderr.write("Unable to create AllClusterCorrections file.\n")
 
 # Now process any other files specified by the user. (By default, TermRenderings.xml and BookNames.xml.)
 otherFiles = re.split(r'\s*,\s*', AdditionalFiles)
@@ -196,7 +310,8 @@ for otherFile in otherFiles:
             sys.stderr.write("Unable to write to file: " + xmlfile + "\n")
             continue
 
-    
+clusallFile.close()
+   
 # Show the user the total tally of changes
 sys.stderr.write("\n\n" + str(totBadChanges) + "\ttotal invalid ZW characters removed.\n" + str(totStdChanges) + "\tclusters standardized.")
 
